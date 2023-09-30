20af0c0ba136baa3fdf056cd01cabcbe660f977e
Darian <116446349+justDarian@users.noreply.github.com>
2023-09-29 21:52:17 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const express = require('express');
-const path = require('path');
+import { createBareServer } from "@tomphttp/bare-server-node";
+import http from "node:http";
+import express from 'express';
+import path from 'node:path';
+import url from 'url';
 
 const app = express();
-const port = 80;
+const server = http.createServer();
+const bareServer = createBareServer("/bare/");
+const port = process.env.PORT || process.argv[2] || 80;
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, '/static'), { extensions: ['html'] }));
 
-const server = app.listen(port, () => {
-  console.log(`DevHub is running on port ${port}`);
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, './static/', '404.html'));
+});
+
+server.on("request", (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on("upgrade", (req, socket, head) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+server.on("listening", () => {
+    console.log(`DevHub running at localhost:${port}`);
+});
+
+server.listen({
+    port: port
 });
